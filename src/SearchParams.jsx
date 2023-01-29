628a3201a24b3ecf15fea313ceb48b30f7b4fa46
parveen232 <parveen297m210@gmail.com>
2023-01-29 12:14:43 +0530

    adding animal breed

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,6 +4,8 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, updateLocation] = useState("");
   const [animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -19,19 +21,39 @@ const SearchParams = () => {
         </label>
         <label htmlFor="animal">
           Animal
-          <select 
-            id="animal" 
-            value={animal} 
+          <select
+            id="animal"
+            value={animal}
             onChange={(e) => {
               updateAnimal(e.target.value);
-            }}>
-              <option />
-              {ANIMALS.map((animal) => (
-                <option key={animal} value={animal}>
-                  {animal}
-                </option>
-              ))}
-            </select>
+              updateBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              updateBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
