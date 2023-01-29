668d98cff2a104eff19e613d551be2b60d54a1c9
parveen232 <parveen297m210@gmail.com>
2023-01-29 11:17:27 +0530

    animal drop down - mapping through data with hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("");
+  const [animal, updateAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -13,6 +16,22 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => updateLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select 
+            id="animal" 
+            value={animal} 
+            onChange={(e) => {
+              updateAnimal(e.target.value);
+            }}>
+              <option />
+              {ANIMALS.map((animal) => (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              ))}
+            </select>
         </label>
         <button>Submit</button>
       </form>
